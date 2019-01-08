ce156a3f6b8414715bd9b008f24f1a50ee42d595
8igMac <michaelshih123@gmail.com>
2019-01-08 11:13:10 +0800

    - lower max_velocity (give particle chance to explore along the way) - cancel init_velociy (reploced by max_velocity) - add some test function

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import random
 import math
 
 # env parameters
-map_margin = 2
-max_velocity = 0.5
-init_velocity = 0.05
-num_particle = 30
-num_one_side_neighbor = 2
+map_margin = 10
+max_velocity = 0.07
+num_particle = 100
+num_one_side_neighbor = 5
 
 # Velocity update parameters
 # c0 = 0.65  # inertia coefficient
@@ -22,9 +21,13 @@ num_one_side_neighbor = 2
 # c1 = 3.3  # personal_best coefficient
 # c2 = 0.7  # local_best coefficient
 
-c0 = 0.4  # inertia coefficient
-c1 = 0.2  # personal_best coefficient
-c2 = 0.8  # local_best coefficient
+# c0 = 0.8  # inertia coefficient
+# c1 = 2  # personal_best coefficient
+# c2 = 0.5  # local_best coefficient
+
+c0 = 1  # inertia coefficient
+c1 = 2  # personal_best coefficient
+c2 = 2  # local_best coefficient
 
 # Target function parameters.
 x0 = 0.2
@@ -54,8 +57,8 @@ class Particle:
                 self.position.x, self.position.y)
         # Init random velocity.
         self.velocity = Velocity()
-        self.velocity.x = random.uniform(-init_velocity, init_velocity)
-        self.velocity.y = random.uniform(-init_velocity, init_velocity)
+        self.velocity.x = random.uniform(-max_velocity, max_velocity)
+        self.velocity.y = random.uniform(-max_velocity, max_velocity)
         # Init personal best
         self.personal_best = self.position
 
@@ -94,15 +97,38 @@ class Particle:
 
 # Target function.
 def eval_fitness(x, y):
+    # Origin function in the spec
     # return \
     #     np.exp(-((x - x0)**2 + (y - y0)**2) / r**2) \
     #   * np.sin(a*x)**2 \
     #   * np.sin(b*x + c*y + d*x**2)**2
+
+    # Some tuning of origin function
     return \
         np.exp(-((x - x0)**2 + (y - y0)**2) / r**2) \
       * np.sin(a*x)**2 \
       * np.sin(b*x)**2 \
       * np.sin(b*x + c*y**2 + d*x**3)**8
+
+    # Rastrigin function
+    # return -( \
+    #     10*2 \
+    #   + (x**2 - 20*np.cos(2*math.pi*x)) \
+    #   + (y**2 - 20*np.cos(2*math.pi*y)) )
+
+    # Easom function
+    # return ( \
+    #     np.cos(x) \
+    #   * np.cos(y) \
+    #   * np.exp(-((x-math.pi)**2 + (y-math.pi)**2)) \
+    # )
+
+    # Tree-hump camel function
+    # return 0.0001 * ( np.abs(\
+    #     np.sin(x) * np.sin(y) * np.exp(np.abs(\
+    #         100 - np.sqrt(x**2 + y**2)/math.pi))\
+    #     ) + 1) ** 0.1
+
 
 def get_global_best(group):
     best = Position()
